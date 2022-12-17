e458449d78890534bf54779163222510ca1d9a20
Juravl <alexey.jurov@gmail.com>
2022-12-17 19:48:32 +0700

    Добавьте файлы проекта.

---
+++ TernOp3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	int a;
+	int b;
+	int c;
+
+	printf("Three different numbers are given. Find two numbers such that the modulus of the difference between them is maximum.\nInput 3 numbers: ");
+	scanf("%d %d %d", &a, &b, &c);
+
+
+}
