57666d5ec119aa5b22eb3a5f5a1765a3654d443f
Juravl <alexey.jurov@gmail.com>
2022-12-17 20:17:36 +0700

    finished work

--- TernOp3.cpp
+++ TernOp3.cpp
@@ -7,19 +7,21 @@ void main()
 	int b;
 	int c;
 
-	int one;
-	int two;
+	int one = 0;
+	int two = 0;
 	int dif = 0;
 
 	printf("Three different numbers are given. Find two numbers such that the modulus of the difference between them is maximum.\nInput 3 numbers: ");
 	scanf("%d %d %d", &a, &b, &c);
 
-	dif = (abs(a - b) > dif) ? abs(a - b), one = a, two = b : dif;
-	dif = (abs(a - c) > dif) ? abs(a - c), one = a, two = c : dif;
-	dif = (abs(b - a) > dif) ? abs(b - a), one = b, two = a : dif;
-	dif = (abs(b - c) > dif) ? abs(b - c), one = b, two = c : dif;
-	dif = (abs(c - a) > dif) ? abs(c - a), one = c, two = a : dif;
-	dif = (abs(c - b) > dif) ? abs(c - b), one = c, two = b : dif;
+	printf("%d\n", abs(a - b));
+
+	dif = (abs(a - b) > dif) ? one = a, two = b, abs(a - b) : dif;
+	dif = (abs(a - c) > dif) ? one = a, two = c, abs(a - c) : dif;
+	dif = (abs(b - a) > dif) ? one = b, two = a, abs(b - a) : dif;
+	dif = (abs(b - c) > dif) ? one = b, two = c, abs(b - c) : dif;
+	dif = (abs(c - a) > dif) ? one = c, two = a, abs(c - a) : dif;
+	dif = (abs(c - b) > dif) ? one = c, two = b, abs(c - b) : dif;
 
 	printf("The numbers are %d and %d. Absolute value of difference is %d.", one, two, dif);
 }
